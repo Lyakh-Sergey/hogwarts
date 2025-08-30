39a109023ba957a6323752879708501b7b6af01a
Lyakh-Sergey <me@ljah.ru>
2025-08-30 23:39:16 +0300

    Logging and configuration is done.

--- school/src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
+++ school/src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
@@ -1,10 +1,14 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.AvatarRepository;
@@ -23,6 +27,8 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 @Service
 @Transactional
 public class AvatarServiceImpl implements AvatarServiceInterface {
+    private static final Logger logger = LoggerFactory.getLogger(AvatarServiceImpl.class);
+
     private final AvatarRepository avatarRepository;
     private final StudentRepository studentRepository;
 
@@ -36,13 +42,20 @@ public class AvatarServiceImpl implements AvatarServiceInterface {
 
     @Override
     public List<Avatar> getAvatarsByPage(int page, int size) {
+        logger.info("Was invoked method for getting avatars by page: page={}, size={}", page, size);
         PageRequest pageRequest = PageRequest.of(page, size);
         return avatarRepository.findAllAvatars(pageRequest);
     }
 
     @Override
     public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
-        Student student = studentRepository.getById(studentId);
+        logger.info("Was invoked method for uploading avatar for student id={}", studentId);
+
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> {
+                    logger.error("Student with id={} not found", studentId);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found");
+                });
 
         Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(avatarFile.getOriginalFilename()));
         Files.createDirectories(filePath.getParent());
@@ -63,9 +76,11 @@ public class AvatarServiceImpl implements AvatarServiceInterface {
         avatar.setData(generateDataForDB(filePath));
 
         avatarRepository.save(avatar);
+        logger.debug("Avatar for student id={} successfully saved", studentId);
     }
 
     private byte[] generateDataForDB(Path filePath) throws IOException {
+        logger.debug("Generating preview for avatar");
         try (InputStream is = Files.newInputStream(filePath);
              BufferedInputStream bis = new BufferedInputStream(is, 1024);
              ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
@@ -83,6 +98,7 @@ public class AvatarServiceImpl implements AvatarServiceInterface {
     }
 
     public Avatar findAvatar(Long studentId) {
+        logger.info("Was invoked method for finding avatar by student id={}", studentId);
         return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
     }
 
